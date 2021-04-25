b9dd879d917338cb04c12db8f9c63136c6cfee24
Smita Shetty <smita.narasimha@gmail.com>
2021-04-25 14:22:01 +1000

    updated-gitignore

--- dist/common/utils/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WeekDaysLimit = exports.WeekDays = void 0;
-exports.WeekDays = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
-exports.WeekDaysLimit = 7;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uc3RhbnRzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vc3JjL2NvbW1vbi91dGlscy9jb25zdGFudHMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQWEsUUFBQSxRQUFRLEdBQUMsQ0FBQyxRQUFRLEVBQUMsU0FBUyxFQUFDLFdBQVcsRUFBQyxVQUFVLEVBQUMsUUFBUSxFQUFDLFVBQVUsRUFBQyxRQUFRLENBQUMsQ0FBQztBQUNsRixRQUFBLGFBQWEsR0FBQyxDQUFDLENBQUEifQ==
